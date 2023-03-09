4f082706b6619414693203c24baaa733c3ede5a3
Jembar Mahdinata <115057644+jemb22286si@users.noreply.github.com>
2023-03-09 21:50:58 +0700

    Add files via upload

---
+++ jembar_pratikum03/for_looping.php
@@ -0,0 +1,19 @@
+<?php
+for($i = 0;$i < 10;$i++){
+    echo "I Love You cantik Muachh <br>";
+
+}
+echo "<br>";
+
+    $array = ['Kucing', 'Anjing', 'Kuda', 'domba'];
+    //count()berfungsi untuk menghitung panjang array
+    for($i = 0;$i < count($array);$i++) {
+        echo $array[$i] . "<br>";
+    }
+
+//for($i = 0;$i < 100;$i++){
+   // echo "Hello SI07, ini looping ke-$i <br>";
+
+//}
+
+
---
+++ jembar_pratikum03/froach_looping.php
@@ -0,0 +1,6 @@
+<?php
+$arrayHewan = ['kepiting', 'kelinci', 'kambing', 'kura-kura'];
+
+foreach($arrayHewan as $key => $value){
+    echo "$key adalah $value <br>";
+}
