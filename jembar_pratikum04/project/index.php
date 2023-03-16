a62db17eeb31e4e801081cca2355584ab1d5ac2d
Jembar Mahdinata <115057644+jemb22286si@users.noreply.github.com>
2023-03-16 21:07:33 +0700

    Add files via upload

---
+++ jembar_pratikum04/project/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <?php require "components/head.php" ?>
+    </head>
+    <body class="sb-nav-fixed">
+    <?php include_once "components/navbar.php" ?>
+    <?php include_once "components/sidebar.php" ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
